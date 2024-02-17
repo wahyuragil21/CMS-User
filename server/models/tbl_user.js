32e8e5a1a99b02b821eea4724716fb5db79159af
wahyuragil21 <why.yuragil@gmail.com>
2024-02-17 13:48:40 +0700

    Feat : Setup configurasi to database

---
+++ server/models/tbl_user.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tbl_user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tbl_user.init({
+    username: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Name is required!'
+        },
+        notNull: {
+          msg: 'Name is required!'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING(54),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Password is required!'
+        },
+        notNull: {
+          msg: 'Password is required!'
+        },
+        len: {
+          args: [5, 8],
+          msg: 'Password must be between 5 and 8 characters!'
+        }
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'tbl_user',
+  });
+  return tbl_user;
+};
